18f18fc185d2b199acdd81d6561b289f0359350f
Shane Spencer <sspencer@abrinc.com>
2014-06-10 13:30:37 -0800

    initial work

--- tempwork/__init__.py
+++ tempwork/__init__.py
@@ -6,13 +6,13 @@ tempwork
 Temporary File/Directory Wrapper with Auto-Cleaning
 """
 
-__name__ = "pglet"
+__name__ = "tempwork"
 __author__ = 'Shane R. Spencer'
 __author_email__ = "shane@bogomip.com"
 __license__ = 'MIT'
 __copyright__ = '2014 Shane R. Spencer'
 __version__ = '0.0.1'
 __status__ = "Prototype"
-__url__ = "https://github.com/whardier/pglet"
-__description__ = "Browser based PostgreSQL data processor"
+__url__ = "https://github.com/whardier/tempwork"
+__description__ = "Temporary File/Directory Wrapper with Auto-Cleaning"
 
--- tempwork/__main__.py
+++ tempwork/__main__.py
@@ -5,16 +5,75 @@ from __future__ import print_function
 import os
 import sys
 
-import tempwork
+import logging
 
 import argparse
+
+import tempfile
+
+import atexit
+import shutil
+
+import datetime
+
+import subprocess
+
+import tempwork
+
+TEMPDIRS = []
+TEMPFILES = []
+
+##    ╻  ┏━┓┏━╸┏━╸╻┏┓╻┏━╸┏━┓┏━╸╺┳╸╻ ╻┏━┓┏━┓┏━╸╺┳╸╻┏━┓┏┓╻
+##    ┃  ┃ ┃┃╺┓┃╺┓┃┃┗┫┃╺┓┗━┓┣╸  ┃ ┃ ┃┣━┛┣━┫┃   ┃ ┃┃ ┃┃┗┫
+## ╺━╸┗━╸┗━┛┗━┛┗━┛╹╹ ╹┗━┛┗━┛┗━╸ ╹ ┗━┛╹  ╹ ╹┗━╸ ╹ ╹┗━┛╹ ╹
+
+class _LoggingSetupAction(argparse.Action):
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values)
+
+
+        logger = logging.getLogger()
+        logger.setLevel(getattr(logging, values.upper()))
+
+## ┏━╸╻  ┏━╸┏━┓┏┓╻╻ ╻┏━┓
+## ┃  ┃  ┣╸ ┣━┫┃┗┫┃ ┃┣━┛
+## ┗━╸┗━╸┗━╸╹ ╹╹ ╹┗━┛╹
+
+def cleanup():
+    for tempfile in TEMPFILES:
+        try:
+            os.remove(tempfile)
+        except:
+            print(sys.exc_info())
+    for tempdir in TEMPDIRS:
+        try:
+            shutil.rmtree(tempdir)
+        except:
+            print(sys.exc_info())
 
 ## ┏┳┓┏━┓╻┏┓╻
 ## ┃┃┃┣━┫┃┃┗┫
 ## ╹ ╹╹ ╹╹╹ ╹
 
 def main():
-    print('hi')
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=tempwork.__description__
+    )
+
+    parser.register('action', 'logging_setup', _LoggingSetupAction)
+
+    parser.add_argument('--logging', action='logging_setup', default='none', help='debug|info|warning|error|none')
+
+    parser.add_argument('--test', action='append')
+
+    parser.add_argument('command')
+    parser.add_argument('command_args', nargs='*')
+
+    args = parser.parse_args()
+    logging.info(repr(args))
+
     return
 
 if __name__ == '__main__':
